a85379c0cf54dc5a5457383a7359448d25acf965
einar <einar.j.holsbo@uit.no>
2023-05-11 08:48:39 +0200

    refactoring

--- ranker.jl
+++ ranker.jl
@@ -3,20 +3,11 @@ using CSV
 reader = CSV.File("record.csv", header=0)
 
 initial_rank = 800.0
-K = 30 # https://en.wikipedia.org/wiki/Elo_rating_system
 
-ranking = Dict()
-for row in reader
-  if !haskey(ranking, row[1]) 
-    ranking[row[1]] = initial_rank 
-  end
-  if !haskey(ranking, row[2]) 
-    ranking[row[2]] = initial_rank 
-  end
-  
-  # https://en.wikipedia.org/wiki/Elo_rating_system
-  r_w = ranking[row[1]]
-  r_b = ranking[row[2]]
+# https://en.wikipedia.org/wiki/Elo_rating_system
+function elo_update(white_rank, black_rank, outcome, K=30)
+  r_w = white_rank
+  r_b = black_rank
 
   q_w = 10^(r_w/400)
   q_b = 10^(r_b/400)
@@ -24,20 +15,37 @@ for row in reader
   e_w = q_w/(q_w + q_b)
   e_b = 1 - e_w
 
-  if row[3] == "1-0"
+  if outcome == "1-0"
     s_w = 1
-  elseif row[3] == "0-1"
+  elseif outcome == "0-1"
     s_w = 0
-  elseif row[3] == "1/2-1/2"
+  elseif outcome == "1/2-1/2"
     s_w = .5
   else
-    error("Bad score: " * row[3])
+    error("Invalid outcome: " * outcome)
   end
 
   s_b = 1 - s_w
 
-  ranking[row[1]] = r_w + K*(s_w - e_w)
-  ranking[row[2]] = r_b + K*(s_b - e_b)
+  return [r_w + K*(s_w - e_w), r_b + K*(s_b - e_b)]
+end
+
+ranking = Dict()
+for row in reader
+  white = row[1]
+  black = row[2]
+  outcome = row[3]
+
+  if !haskey(ranking, white) 
+    ranking[white] = initial_rank 
+  end
+  if !haskey(ranking, black) 
+    ranking[black] = initial_rank 
+  end
+  
+  new_scores = elo_update(ranking[white], ranking[black], outcome)
+  ranking[white] = new_scores[1]
+  ranking[black] = new_scores[2]
 end
 
 ranking = sort!(collect(ranking), by=last, rev=true)
