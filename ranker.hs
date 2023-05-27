dd23784409e77be4825a54d34f602b7d3b0201cd
einar <einar.j.holsbo@uit.no>
2023-05-27 14:32:53 +0200

    input mostly done

--- ranker.hs
+++ ranker.hs
@@ -1,6 +1,10 @@
+import System.Environment -- Some I/O stuff
+
 initialScore = 800.0
 maxAdjustment = 30.0
 
+-- expected score is a logistic function of the difference in ratings
+-- can be interpreted as a predicted probability of winning
 expectedScore :: Float -> Float -> Float
 expectedScore ratingA ratingB = 1/(1 + 10**(diff/400))
   where diff = ratingB - ratingA
@@ -11,6 +15,9 @@ expectedScore ratingA ratingB = 1/(1 + 10**(diff/400))
 --     = k((1-sa) - (1-ea)) 
 --     = rb + k(1 -sa -1 +ea) 
 --     = rb - k(sa - ea)
+--
+-- we see that it is a linear update of the rating based on the difference
+-- in expected score and actual score
 adjustment :: Float -> Float -> Float
 adjustment expected actual = maxAdjustment*(actual - expected)
 
@@ -19,4 +26,40 @@ updateScore :: Float -> Float -> Float -> (Float, Float)
 updateScore ratingA ratingB outcome = (ratingA + adj, ratingB - adj)
   where adj = adjustment (expectedScore ratingA ratingB) outcome
 
-main = putStrLn $ show (updateScore 800 800 1)
+
+-- trim whitespaces on ends of strings
+-- not in use atm....
+trim :: String -> String
+trim = reverse . stripLeft . reverse . stripLeft
+  where
+    stripLeft s
+      | s == " " || s == "" = ""
+      | head s == ' '       = stripLeft . tail $ s
+      | otherwise           = s
+
+-- splits a record on its commas
+splitComma :: String -> [String]
+splitComma "" = []
+splitComma str = taken : (splitComma rest)
+  where 
+    term = takeWhile (/= ',')
+    taken = term str
+    rest = drop ((length taken) + 1) str
+
+-- takes a line from the records "Player A, Player B, 1-0" and spits out
+-- (("Player A", "Player B"), 1.0)
+recordToTuple :: String -> ((String, String), Float)
+recordToTuple rec = tuplefy . splitComma $ rec
+  where
+    tuplefy [a, b, c] = ((a, b), score c)
+    score "1/2-1/2" = 0.5
+    score "1-0" = 1
+    score "0-1" = 0
+
+
+main = do
+  contents <- readFile "record.csv"
+
+  let matches = lines contents
+
+  putStrLn $ show (map recordToTuple matches)
