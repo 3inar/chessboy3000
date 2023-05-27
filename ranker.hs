7203f1b31789c29ef1e514a1a1495db36fd5056e
einar <einar.j.holsbo@uit.no>
2023-05-27 20:37:34 +0200

    add data type, refactor some

--- ranker.hs
+++ ranker.hs
@@ -1,7 +1,16 @@
 import System.Environment -- Some I/O stuff
+import Data.List (nub) -- remove duplicates from list
 
 initialScore = 800.0
 maxAdjustment = 30.0
+
+data Match = Match
+  { white :: String, 
+    black :: String, 
+    result :: Float
+  }
+
+-- Now that our different fields have names, each name actually acts as a function allows us to extract that value from the larger object. We no longer have to deconstruct the entire object via pattern matching to get each value out.
 
 -- expected score is a logistic function of the difference in ratings
 -- can be interpreted as a predicted probability of winning
@@ -28,38 +37,46 @@ updateScore ratingA ratingB outcome = (ratingA + adj, ratingB - adj)
 
 
 -- trim whitespaces on ends of strings
--- not in use atm....
-trim :: String -> String
-trim = reverse . stripLeft . reverse . stripLeft
-  where
-    stripLeft s
-      | s == " " || s == "" = ""
-      | head s == ' '       = stripLeft . tail $ s
-      | otherwise           = s
+-- not in use atm.
+-- trim :: String -> String
+-- trim = reverse . stripLeft . reverse . stripLeft
+--   where
+--     stripLeft s
+--       | s == " " || s == "" = ""
+--       | head s == ' '       = stripLeft . tail $ s
+--       | otherwise           = s
 
 -- splits a record on its commas
 splitComma :: String -> [String]
 splitComma "" = []
-splitComma str = taken : (splitComma rest)
+splitComma str = term : (splitComma rest)
   where 
-    term = takeWhile (/= ',')
-    taken = term str
-    rest = drop ((length taken) + 1) str
+    term = takeWhile (/= ',') str
+    rest = drop ((length term) + 1) str
 
 -- takes a line from the records "Player A, Player B, 1-0" and spits out
 -- (("Player A", "Player B"), 1.0)
-recordToTuple :: String -> ((String, String), Float)
-recordToTuple rec = tuplefy . splitComma $ rec
+recordToMatch :: String -> Match
+recordToMatch rec = let [a, b, c] = splitComma $ rec in Match a b (score c)
   where
-    tuplefy [a, b, c] = ((a, b), score c)
-    score "1/2-1/2" = 0.5
     score "1-0" = 1
     score "0-1" = 0
+    score "1/2-1/2" = 0.5
 
+-- want to use foldr somehow and take like ranking
+-- updateRating :: [(String, Float)] -> ((String, String), Float) -> [(String, Float)]
+-- updateRating = do
+  
+
+-- pull out unique player names from the set of matches
+uniquePlayers :: [Match] -> [String]
+uniquePlayers ls = nub $ foldr (++) [] $ map extractNames ls
+  where 
+    extractNames match = [white match, black match]
 
 main = do
   contents <- readFile "record.csv"
 
-  let matches = lines contents
+  let matches = map recordToMatch (lines contents)
 
-  putStrLn $ show (map recordToTuple matches)
+  putStrLn $ show (uniquePlayers matches)
