2cf376e5804b69ddd4cd3f6ae0a735cc6521010e
einar <einar.j.holsbo@uit.no>
2024-03-10 11:38:24 +0100

    list processing

--- ranker.rkt
+++ ranker.rkt
@@ -9,15 +9,6 @@
          (map string-trim (string-split x ",")))
        record))
 
-; get list of players for a set of games
-(define (extract-players games)
-  (remove-duplicates 
-    (foldr (lambda (x res) (append (take x 2) res)) '() games)))
-
-(define players (extract-players games))
-(define active ; people who have played in the last 50 games
-  (let ([n_drop (- (length games) 50)]) 
-    (extract-players (drop games n_drop))))
 
 ; love some mutable state
 (define ratings (make-hash))
@@ -29,10 +20,10 @@
 
   ; score for white and black 
   (define sw (cond
-     [(equal? (third gm) "1-0") 1]
-     [(equal? (third gm) "0-1") 0]
-     [(equal? (third gm) "1/2-1/2") .5]
-     [else (error "invalid match outcome")]))
+               [(equal? (third gm) "1-0") 1]
+               [(equal? (third gm) "0-1") 0]
+               [(equal? (third gm) "1/2-1/2") .5]
+               [else (error "invalid match outcome")]))
   (define sb (- 1 sw))
 
   ; used in calculating expected scores
@@ -44,6 +35,7 @@
   (define eb (- 1 ew))
   
   ; rating update is difference in actual score and expected score scaled by K
+  ; love a good side effect
   (hash-set! ratings (first gm) (+ rating-white (* K (- sw ew))))
   (hash-set! ratings (second gm) (+ rating-black (* K (- sb eb)))))
 
@@ -70,8 +62,42 @@
 ;           string(Int(round(ranking[i][2]))))
 ; end
 
+; pad string w spaces to target length
+(define (pad str len) 
+  (if (<= len (string-length str))
+      str 
+      (pad (string-join (list str " ") "") len)))
 
-; print scores
-(define longest_name (foldl max -1 (map string-length active)))
+; get list of players for a set of games
 
-; TODO: filter on active &c.
+; list of active players: people who played in the last 50 games
+; this is kind of ugly?? used (let ()) before. maybe better?
+(define active ((lambda ()
+  (define n_drop (- (length games) 50)) 
+  (define (extract-players gms)
+    (remove-duplicates 
+      (foldr (lambda (x res) (append (take x 2) res)) '() gms)))
+  (extract-players (drop games n_drop)))))
+
+
+(define (positions n_players)
+  (map (lambda (x) (string-join (list (~r x) ".") ""))
+       (range 1 (+ n_players 1))))
+
+; filter inactive players out
+(define ranked (filter (lambda (x) (member (car x) active)) ranking))
+
+(displayln "A218B/HDL chess federation official ranking")
+(displayln "-------------------------------------------")
+
+(for-each (lambda (pos player) 
+            (define longest_name (apply max (map string-length active)))
+            (define ppos (pad pos 3)) ; must change when we get 100 players
+            (define pplayer (pad (car player) longest_name))
+            (define rating (~r (exact-round (cdr player))))
+            (displayln (string-join (list ppos pplayer rating)))) 
+          (positions (length ranked))
+          ranked)
+; string-join
+; exact-round
+; (~r 4) -> "4"
